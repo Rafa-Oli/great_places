fba8f6e288d271f821660eb3841ba83c82e99c65
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-03-13 21:31:44 -0300

    enhancing places listing screen

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/greate_place.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Places'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM);
-            },
-          )
-        ],
-      ),
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('My Places'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM);
+              },
+            )
+          ],
+        ),
+        body: Consumer<GreatPlaces>(
+          child: Center(child: Text('No places registered!')),
+          builder: (context, greatPlaces, child) => greatPlaces.itemsCount == 0
+              ? child
+              : ListView.builder(
+                  itemCount: greatPlaces.itemsCount,
+                  itemBuilder: (ctx, i) => ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          FileImage(greatPlaces.itemByIndex(i).image),
+                    ),
+                    title: Text(greatPlaces.itemByIndex(i).title),
+                    onTap: () {},
+                  ),
+                ),
+        ));
   }
 }
