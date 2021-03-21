a1d7d3dfbabf5cb17c2cc87fdd06e8a57802b52b
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-03-20 22:36:49 -0300

    working with localization

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:great_places/providers/greate_place.dart';
 import 'package:great_places/widgets/image_input.dart';
+import 'package:great_places/widgets/location_input.dart';
 import 'package:provider/provider.dart';
 
 class PlaceFormScreen extends StatefulWidget {
@@ -56,6 +57,10 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
                         height: 10,
                       ),
                       ImageInput(this._selectImage),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      LocationInput(),
                     ],
                   ),
                 ),
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageURL;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          child: _previewImageURL == null
+              ? Text('Location not specified!')
+              : Image.network(
+                  _previewImageURL,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: null,
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(
+                      Theme.of(context).primaryColor)),
+              icon: Icon(Icons.location_on),
+              label: Text('Current location'),
+            ),
+            TextButton.icon(
+              onPressed: null,
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(
+                      Theme.of(context).primaryColor)),
+              icon: Icon(Icons.map),
+              label: Text('Select on the map'),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
